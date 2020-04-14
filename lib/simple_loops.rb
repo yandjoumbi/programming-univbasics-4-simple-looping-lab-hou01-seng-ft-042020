799180eee80447733967edffd1f4801aa3e082ac
Yannick <ydjoumbi@gmail.com>
2020-04-14 05:06:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
 # Write your methods here
+
+def loop_message_five_times (message ="Hello World./n")
+  5.times 
+  puts message
+end
+
+
+def loop_message_n_times (message, number)
+  number.times 
+  puts message
+end
+
+
+def output_array (array, number)
+  
+end
+
+
+def return_string_array(array)
+  arry.to_s
+end
