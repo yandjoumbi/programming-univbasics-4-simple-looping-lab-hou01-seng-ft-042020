67741e03ce9c6df341abb112f7cdb98a57850be2
Yannick <ydjoumbi@gmail.com>
2020-04-14 05:08:45 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,14 +1,14 @@
 # Write your methods here
 
-def loop_message_five_times (message ="Hello World./n")
+def loop_message_five_times (message)
   5.times 
-  puts message
+  
 end
 
 
 def loop_message_n_times (message, number)
   number.times 
-  puts message
+  
 end
 
 
